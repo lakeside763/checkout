8f981e3286d058eaf43097ebf1335a0acafa6424
lakeside763 <lakeside763@gmail.com>
2021-12-28 13:20:01 +0100

    fix: component resolving error

--- tests/components/products.test.jsx
+++ tests/components/products.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Products from '../../components/products/Products';
+import Products from './../../components/products/Products';
 import { cleanup, render, screen } from '@testing-library/react';
 import renderer from 'react-test-renderer';
 import products from './../../products.json';
